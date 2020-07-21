c8ad2782638ea7edc3b6cae81bae748ce78bef3d
Mirza Golam Abbas Shahneel <mgashahneel@gmail.com>
2020-07-21 20:05:50 +0600

    Add newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -14,6 +14,26 @@ app.use(express.static(publicPath));
 
 io.on('connection',(socket)=>{
     console.log('New user connected');
+    /* socket.emit('newEmail',{
+        from: 'shah@reve.com',
+        text: 'Hey! What\'s going on',
+        createdAt: 123
+    }); */
+
+    socket.emit('newMessage',{
+        from: 'Kopila',
+        text: 'Amare loiba majhi?',
+        createdAt: 123
+    });
+
+/*     socket.on('createEmail',(newEmail)=>{
+        console.log('createEmail',newEmail);
+    }); */
+
+    socket.on('createMessage',(message)=>{
+        console.log('createMessage',message);
+    });
+
     socket.on('disconnect',()=>{
         console.log('User was disconnected');
     });    
