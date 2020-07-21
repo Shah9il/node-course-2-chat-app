9f355d22e93d644d5a7ec2b0aac47b994b229502
Mirza Golam Abbas Shahneel <mgashahneel@gmail.com>
2020-07-21 16:32:08 +0600

    Add Connect and Disconnect Event Handlers

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.use(express.static(publicPath));
 io.on('connection',(socket)=>{
     console.log('New user connected');
     socket.on('disconnect',()=>{
-        console.log('Disconnected from client');
+        console.log('User was disconnected');
     });    
 });
 
