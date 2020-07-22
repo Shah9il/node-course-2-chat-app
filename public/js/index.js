6107c6b7004d671af3e5dd449f7b9ea4f0594f5c
Mirza Golam Abbas Shahneel <mgashahneel@gmail.com>
2020-07-22 08:05:41 +0600

    Add MustacheJS for message templates

--- public/js/index.js
+++ public/js/index.js
@@ -9,20 +9,40 @@ socket.on('disconnect',function (){
 
 socket.on('newMessage',function(message){
     var formattedTime = moment(message.createdAt).format('h:mm a');
+    var template = jQuery('#message-template').html();
+    var html = Mustache.render(template,{
+        from: message.from,
+        text: message.text,
+        createdAt: formattedTime
+    });
+
+    jQuery('#messages').append(html);
+
+/*     
     console.log('Got new message',message);
     var li = jQuery('<li></li>');
     li.text(`${message.from} @${formattedTime}: ${message.text}`);
-    jQuery('#messages').append(li);
+    jQuery('#messages').append(li); */
+
 });
 
 socket.on('newLocationMessage',function(message){
     var formattedTime = moment(message.createdAt).format('h:mm a');
-    var li = jQuery('<li></li>');
+    var template = jQuery('#location-message-template').html();
+    var html = Mustache.render(template,{
+        from: message.from,
+        url: message.url,
+        createdAt: formattedTime
+    });
+    
+    jQuery('#messages').append(html);
+    
+    /* var li = jQuery('<li></li>');
     var a = jQuery('<a target="_blank">My Current Location</a>');
     li.text(`${message.from} @${formattedTime}: `);
     a.attr('href',message.url);
     li.append(a);
-    jQuery('#messages').append(li);
+    jQuery('#messages').append(li); */
 })
 
 var messsageTextBox = jQuery('[name=message]');
@@ -47,11 +67,11 @@ locationButton.on('click',function (){
     locationButton.attr('disabled','disabled').text('Sending location');
 
     navigator.geolocation.getCurrentPosition(function (position){
-        locationButton.removeAttr('disabled').text('Send location');;
         socket.emit('createLocationMessage',{
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
+        locationButton.removeAttr('disabled').text('Send location');;
     },function (){
         locationButton.removeAttr('disabled').text('Send location');;
         return alert('Unable to fetch location!');
